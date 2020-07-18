6fe88b01a665dde3add698495e7e0a51cf27a3e1
ksw6169 <ksw6169@naver.com>
2020-07-18 17:51:04 +0900

    [update] 종속성 충돌 해결, .gitignore 설정, Security 우선 제외

--- src/main/java/com/tancheon/tmon/domain/User.java
+++ src/main/java/com/tancheon/tmon/domain/User.java
@@ -3,11 +3,16 @@ package com.tancheon.tmon.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @Entity
 public class User {
 
+    @Id @GeneratedValue
     private long id;
 
 }
