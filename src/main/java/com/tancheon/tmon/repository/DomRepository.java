8a7c9034cfe8eeb44b05bd8a7b3c19d776271481
ksw6169 <ksw6169@naver.com>
2020-07-30 01:01:00 +0900

    [update] DOM 클래스 제거

--- src/main/java/com/tancheon/tmon/repository/DomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tancheon.tmon.repository;
-
-import com.tancheon.tmon.domain.Dom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DomRepository extends JpaRepository<Dom, Long> {
-//public interface DomRepository extends JpaRepository<Dom, String> {
-}
