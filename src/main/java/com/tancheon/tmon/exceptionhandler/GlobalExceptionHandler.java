666b49cb2712151b3e24ff5f77a2096c5520b870
ksw6169 <ksw6169@naver.com>
2020-08-03 00:56:29 +0900

    [update] GlobalExceptionHandler 추가(@RestControllerAdvice)

--- src/main/java/com/tancheon/tmon/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/tancheon/tmon/exceptionhandler/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.tancheon.tmon.exceptionhandler;
 
-/*import org.springframework.web.bind.annotation.ExceptionHandler;*/
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/*@ExceptionHandler*/
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
-
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> defaultExceptionHandle(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
